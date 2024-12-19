b331d8a1d94dfa337c90a0f7dff0e6b1beb1a3ef
Роман Желонкин <xcision@ya.ru>
2024-12-20 00:20:12 +0300

    add access

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from .api.views import router
 from pathlib import Path
+from fastapi.middleware.cors import CORSMiddleware
 import sys
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",  # Разрешаем запросы только с этого домена
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Разрешенные источники
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешаем все методы
+    allow_headers=["*"],  # Разрешаем все заголовки
+)
+
 app.include_router(router)
